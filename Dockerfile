9b0c9e575c99f64976353a621740a989d0b75e9a
DMontgomery40 <david.montgomery@jobot.com>
2024-12-15 13:17:13 -0700

    update to new name

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.11-slim
 
-WORKDIR /mynpu
-COPY mynpu.py ./mynpu.py
+WORKDIR /intel-npu-top
+COPY intel-npu-top.py ./intel-npu-top.py
 
-CMD ["python3", "mynpu.py"]
+CMD ["python3", "intel-npu-top.py"]
--- debian/setup.py
+++ debian/setup.py
@@ -4,7 +4,9 @@ setup(
     name="intel-npu-top",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "setuptools>=42.0.0",
+    ],
     entry_points={
         'console_scripts': [
             'intel-npu-top=intel-npu-top.cli:main',
@@ -15,7 +17,7 @@ setup(
     description="A monitoring tool for Intel NPUs",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/DMontgomery4/intel-npu-top",
+    url="https://github.com/DMontgomery40/intel-npu-top",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
